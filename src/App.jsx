8daafaa5f004a59be9dd8af8e71c657445f4fae3
ben-dh3 <benjaminduffield1997@gmail.com>
2022-10-19 17:54:03 +0100

    add project

--- src/App.jsx
+++ src/App.jsx
@@ -6,73 +6,76 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { Vector2 } from 'three';
 
 
-//function is run when the page first loads
+
 
 function App() {
+
+  //function is run when the page first loads
 
   useEffect(() => { 
   const scene = new THREE.Scene();
 
   const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  camera.position.setZ(100);
+  camera.position.setZ(50);
 
-  //
+  //Renderer
 
   const canvas = document.getElementById('my-canvas');
   const renderer = new THREE.WebGLRenderer({
     canvas,
     antialias: true
   });
-
-  //
-
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
   document.body.appendChild(renderer.domElement);
 
-  //
+  //Controls
 
   const controls = new OrbitControls(camera, renderer.domElement); 
   const pointer = new THREE.Vector2();
+
+
   
   function onPointerMove( event ) {
 
-	pointer.x =  ( event.clientX / window.innerWidth ) * 2 - 1;
-
-	pointer.y =  ( event.clientY / window.innerHeight ) * 2 + 1;
+	pointer.x =  ( event.clientX / window.innerWidth ) * 1.5;
+	pointer.y =  ( event.clientY / window.innerHeight ) * -1.5;
 
   };
 
-  //
+  //Light
 
   const ambientLight = new THREE.AmbientLight(0xffffff, 2); //color and intensity
   scene.add(ambientLight);
 
-  //
-
+  //GLTF load
+  
   let loadedModel;
   const gltfLoader = new GLTFLoader();
-  gltfLoader.load('./ethlogo.gltf', (gltfScene) => {
+  gltfLoader.load('./cube.gltf', (gltfScene) => {
     loadedModel = gltfScene;
-    
     scene.add(gltfScene.scene);
   });
 
   //
 
   const animate = () => {
-    if (loadedModel) {
-    loadedModel.scene.scale.set(10, 10, 10);    
-
-    camera.position.y = THREE.MathUtils.lerp(camera.position.y, (pointer.y * Math.PI) / 0.2, 0.1);
-    camera.position.x = THREE.MathUtils.lerp(camera.position.x, (pointer.x * Math.PI) / -0.01, 0.1);
+    if (loadedModel) {   
+    loadedModel.scene.scale.set(10, 10, 10);
     
+    loadedModel.scene.position.y = THREE.MathUtils.lerp(loadedModel.scene.position.y, (pointer.y * Math.PI) / 0.6, 0.1);
+    loadedModel.scene.position.x = THREE.MathUtils.lerp(loadedModel.scene.position.x, (pointer.x * Math.PI) / 0.6, 0.1);
     
     }
+
     controls.update();
+
     renderer.render(scene, camera);
+
     window.addEventListener( 'pointermove', onPointerMove );
-    window.requestAnimationFrame(animate);
+
+    window.requestAnimationFrame( animate );
+
     };
   
   animate();
